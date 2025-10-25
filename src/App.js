5b5ee277250550906df4b86fa14bd0ace9d15912
KJWGarden <mike03131313@gmail.com>
2025-10-25 23:31:04 +0900

    feat: 이름 예외처리
    구분자로 파싱된 이름이 5자보다 넘어가면 예외처리 구현

--- src/App.js
+++ src/App.js
@@ -28,6 +28,9 @@ class App {
 
     const carSet = new Set();
     for (const name of names) {
+      if (name.length > 5) {
+        throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+      }
       carSet.add(name);
     }
 

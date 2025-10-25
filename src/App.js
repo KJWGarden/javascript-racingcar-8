6b3f1a452e97bd9f87613eafbf2e21ed9684242e
KJWGarden <mike03131313@gmail.com>
2025-10-25 22:07:12 +0900

    feat: 이름 문자열 파싱
    구분자(,)를 기준으로 이름 문자열을 파싱 후 콜렉션으로 저장

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ class App {
     const nameString = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)"
     );
-    Console.print(nameString);
+    const nameSet = this.parseName(nameString);
+    Console.print(nameSet);
+  }
+  parseName(input) {
+    const names = input.split(",").map((name) => name.trim());
+    return names;
   }
 }
 

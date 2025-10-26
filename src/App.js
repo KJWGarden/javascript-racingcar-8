c6f96331480319062302bb0908d1dd7074f03ecc
KJWGarden <mike03131313@gmail.com>
2025-10-26 21:12:21 +0900

    feat: 구분자 예외처리
    구분자 사이 빈 이름 입력 될 뗴 예외처리 (oo,,xx)

--- src/App.js
+++ src/App.js
@@ -35,12 +35,18 @@ class App {
 
     const carSet = new Set();
     for (const name of names) {
+      if (name === "") {
+        throw new Error("[ERROR] 자동차 이름은 비어 있을 수 없습니다.");
+      }
+
       if (name.length > 5) {
         throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
       }
+
       if (carSet.has(name)) {
         throw new Error("[ERROR] 자동차 이름은 중복될 수 없습니다.");
       }
+
       carSet.add(name);
     }
 
